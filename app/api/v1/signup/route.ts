e305d53dbc7dc1c62758fbd51c49f1c30ab83cf3
harshit-kharayat23 <harshitkharayat15@gmail.com>
2025-10-19 22:38:32 +0530

    did npx prisma generate to generate the client and then to connect it with the backend

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,8 +1,20 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+const client=new PrismaClient();
 
 export  async function POST(req:NextRequest){
      
     const data=await req.json();
+
+   await client.user.create({
+        data:{
+            username:data.username,
+            password:data.password
+        }
+    })
+    
+    
     console.log(data);
     return NextResponse.json({
         messge:"successfully sign up"
